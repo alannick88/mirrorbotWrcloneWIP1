aa40e21645fa1992f4cb3e0a308deb691a7ba584
alannick88 <85379398+alannick88@users.noreply.github.com>
2021-06-24 13:38:16 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copy rclone.conf to path
+mkdir -p /app/.config/rclone
+cp rclone.conf /app/.config/rclone/rclone.conf
+
 # Install rclone static binary
 wget -q https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip -qqj rclone-current-linux-amd64.zip
